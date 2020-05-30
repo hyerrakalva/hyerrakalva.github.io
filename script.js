7d469ad46c62a767ceecd1d19a7035d7d1b391cc
Himan Yerrakalva <yerrak@umich.edu>
2020-05-29 23:39:08 -0400

    Implemented styling and base functionality to experience section
    Formatted this section in a card-like slideshow

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let load_finished = false;
 let skills_visible = false;
+let slide_counter = {
+    "experience": 0,
+    "projects": 0
+};
+let slide_name = {
+    'experience': 'work',
+    'projects': 'project'
+};
+let animation_lock = false;
 
 $(window).on('beforeunload', function() {
     $('body').hide();
@@ -137,3 +146,87 @@ $(window).on("load", function() {
     }, {threshold: [0.25]});
     skills_observer.observe(document.querySelector('.skills ul'))
 });
+
+function set_slide(slide_num, section, direction) {
+    if (animation_lock) {
+        return;
+    }
+    else {
+        animation_lock = true;
+    }
+    if (direction === 100) {
+        console.assert(slide_counter[section] !== undefined);
+        if (slide_counter[section] < slide_num) {
+            direction = 1;
+        }
+        else if (slide_counter[section] > slide_num) {
+            direction = -1;
+        }
+        else {
+            return;
+        }
+    }
+    console.assert(direction === 1 || direction === -1);
+
+    let dots = $('.' + section + ' .dot-container')[0].children;
+    dots[slide_counter[section]].classList.remove('active');
+    dots[slide_num].classList.add('active');
+
+    anime({
+        targets: '.' + section + ' .' + slide_name[section] + '.visible',
+        duration: 250,
+        translateX: [0, -45 * direction],
+        easing: 'linear'
+    });
+    anime({
+        targets: '.' + section + ' .' + slide_name[section] + '.visible',
+        duration: 250,
+        endDelay: 250,
+        opacity: [1, 0],
+        easing: 'linear'
+    }).finished.then(function () {
+        let slides = $('.' + slide_name[section]);
+        slides[slide_counter[section]].classList.remove('visible');
+        slides[slide_num].classList.add('visible');
+        anime({
+            targets: '.' + section + ' .' + slide_name[section] + '.visible img',
+            duration: 1000,
+            scale: [1.1, 1.0],
+            easing: 'easeOutCubic'
+        })
+        anime({
+            targets: '.' + section + ' .' + slide_name[section] + '.visible',
+            duration: 250,
+            translateX: [45 * direction, 0],
+            easing: 'linear'
+        });
+        anime({
+            targets: '.' + section + ' .' + slide_name[section] + '.visible',
+            duration: 250,
+            opacity: [0, 1],
+            easing: 'linear'
+        }).finished.then(function () {
+            animation_lock = false;
+        });
+        slide_counter[section] = slide_num;
+    });
+}
+
+function arrow_navigate(section, direction) {
+    let index_to_navigate = slide_counter[section];
+    let length = document.getElementsByClassName(section)[0].children[2].childElementCount;
+    if (direction === 1) {
+        if (++index_to_navigate === length) {
+            index_to_navigate = 0;
+        }
+    }
+    else if (direction === -1) {
+        if (--index_to_navigate === -1) {
+            index_to_navigate = length - 1;
+        }
+    }
+    else {
+        console.assert(false);
+    }
+    set_slide(index_to_navigate, section, direction);
+}
