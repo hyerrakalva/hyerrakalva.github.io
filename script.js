e1ca6b7973f2c7e2a4098f00b5239b44f62fa211
Himan Yerrakalva <yerrak@umich.edu>
2021-05-03 22:13:33 -0400

    Website now switches theme as soon as system theme is changed

--- script.js
+++ script.js
@@ -193,6 +193,14 @@ $(document).ready(setTimeout(function enter() {
     if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
         document.body.classList.add('dark');
     }
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", e => {
+        if (e.matches) {
+            document.body.classList.add('dark');
+        }
+        else {
+            document.body.classList.remove('dark');
+        }
+    })
     anime({
         targets: '.preload-greeter',
         duration: 500,
